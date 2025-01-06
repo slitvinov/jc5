cf5ddcddf4279744e01db8c0be93bf586aaef778
Sergey Litvinov <slitvinov@gmail.com>
2025-01-07 00:22:24 +0100

    rm junk

--- score_matching.py
+++ score_matching.py
@@ -9,7 +9,10 @@ from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as tt
+
+
 class Digits(Dataset):
+
     def __init__(self, mode='train', transforms=None):
         digits = load_digits()
         if mode == 'train':
@@ -19,6 +22,7 @@ class Digits(Dataset):
         else:
             self.data = digits.data[1350:].astype(np.float32)
         self.transforms = transforms
+
     def __len__(self):
         return len(self.data)
 
@@ -30,40 +34,36 @@ class Digits(Dataset):
 
 
 class ScoreMatching(nn.Module):
-    def __init__(self, snet, alpha, sigma, eta, D, T):
+
+    def __init__(self):
         super(ScoreMatching, self).__init__()
-        print('Score Matching by JT.')
         self.snet = snet
-        self.D = D
-        self.sigma = sigma
-        self.T = T
-        self.alpha = alpha
-        self.eta = eta
 
     def sample_base(self, x_1):
         return 2. * torch.rand_like(x_1) - 1.
 
     def langevine_dynamics(self, x):
-        for t in range(self.T):
-            x = x + self.alpha * self.snet(x) + self.eta * torch.randn_like(x)
+        for t in range(T):
+            x = x + alpha * self.snet(x) + eta * torch.randn_like(x)
         return x
 
     def forward(self, x, reduction='mean'):
         epsilon = torch.randn_like(x)
-        tilde_x = x + self.sigma * epsilon
+        tilde_x = x + sigma * epsilon
         s = self.snet(tilde_x)
-        SM_loss = (1. / (2. * self.sigma)) * ((s + epsilon)**2.).sum(-1)
+        SM_loss = (1. / (2. * sigma)) * ((s + epsilon)**2.).sum(-1)
         if reduction == 'sum':
             loss = SM_loss.sum()
         else:
             loss = SM_loss.mean()
         return loss
 
-    def sample(self,  batch_size=64):
-        x = self.sample_base(torch.empty(batch_size, self.D))
+    def sample(self, batch_size=64):
+        x = self.sample_base(torch.empty(batch_size, D))
         x = self.langevine_dynamics(x)
         x = torch.tanh(x)
         return x
+
 
 def evaluation(test_loader, name=None, model_best=None, epoch=None):
     if model_best is None:
@@ -85,18 +85,6 @@ def evaluation(test_loader, name=None, model_best=None, epoch=None):
     return loss
 
 
-def samples_real(name, test_loader):
-    num_x = 4
-    num_y = 4
-    x = next(iter(test_loader)).detach().numpy()
-    fig, ax = plt.subplots(num_x, num_y)
-    for i, ax in enumerate(ax.flatten()):
-        plottable_image = np.reshape(x[i], (8, 8))
-        ax.imshow(plottable_image, cmap='gray')
-        ax.axis('off')
-    plt.savefig(name+'_real_images.pdf', bbox_inches='tight')
-    plt.close()
-
 def samples_generated(name, data_loader, extra_name='', T=None):
     model_best = torch.load(name + '.model')
     model_best.eval()
@@ -111,51 +99,11 @@ def samples_generated(name, data_loader, extra_name='', T=None):
         plottable_image = np.reshape(x[i], (8, 8))
         ax.imshow(plottable_image, cmap='gray')
         ax.axis('off')
-    plt.savefig(name + '_generated_images' + extra_name + '.pdf', bbox_inches='tight')
+    plt.savefig(name + '_generated_images' + extra_name + '.pdf',
+                bbox_inches='tight')
     plt.close()
 
-def plot_curve(name, nll_val):
-    plt.plot(np.arange(len(nll_val)), nll_val, linewidth='3')
-    plt.xlabel('epochs')
-    plt.ylabel('score matching loss')
-    plt.savefig(name + '_sm_val_curve.pdf', bbox_inches='tight')
-    plt.close()
 
-def training(name, max_patience, num_epochs, model, optimizer, training_loader, val_loader):
-    nll_val = []
-    best_nll = 1000.
-    patience = 0
-    for e in range(num_epochs):
-        model.train()
-        for indx_batch, batch in enumerate(training_loader):
-            loss = model.forward(batch)
-
-            optimizer.zero_grad()
-            loss.backward(retain_graph=True)
-            optimizer.step()
-        loss_val = evaluation(val_loader, model_best=model, epoch=e)
-        nll_val.append(loss_val)
-        if e == 0:
-            print('saved!')
-            torch.save(model, name + '.model')
-            best_nll = loss_val
-        else:
-            if loss_val < best_nll:
-                print('saved!')
-                torch.save(model, name + '.model')
-                best_nll = loss_val
-                patience = 0
-
-                samples_generated(name, val_loader, extra_name="_epoch_" + str(e))
-            else:
-                patience = patience + 1
-
-        if patience > max_patience:
-            break
-
-    nll_val = np.asarray(nll_val)
-
-    return nll_val
 transforms = tt.Lambda(lambda x: 2. * (x / 17.) - 1.)
 train_data = Digits(mode='train', transforms=transforms)
 val_data = Digits(mode='val', transforms=transforms)
@@ -176,18 +124,63 @@ name = 'sm' + '_' + str(T)
 result_dir = name + '/'
 if not (os.path.exists(result_dir)):
     os.mkdir(result_dir)
-snet = nn.Sequential(nn.Linear(D, M), nn.SELU(),
-                     nn.Linear(M, M), nn.SELU(),
-                     nn.Linear(M, M), nn.SELU(),
-                     nn.Linear(M, D), nn.Hardtanh(min_val=-4., max_val=4.))
-model = ScoreMatching(snet=snet, alpha=alpha, sigma=sigma, eta=eta, T=T, D=D)
-optimizer = torch.optim.Adamax([p for p in model.parameters() if p.requires_grad == True], lr=lr)
-nll_val = training(name=result_dir + name, max_patience=max_patience, num_epochs=num_epochs, model=model, optimizer=optimizer,
-                       training_loader=training_loader, val_loader=val_loader)
+snet = nn.Sequential(nn.Linear(D, M), nn.SELU(), nn.Linear(M, M), nn.SELU(),
+                     nn.Linear(M, M), nn.SELU(), nn.Linear(M, D),
+                     nn.Hardtanh(min_val=-4., max_val=4.))
+model = ScoreMatching()
+optimizer = torch.optim.Adamax(
+    [p for p in model.parameters() if p.requires_grad == True], lr=lr)
+
+nll_val = []
+best_nll = 1000.
+patience = 0
+for e in range(num_epochs):
+    model.train()
+    for indx_batch, batch in enumerate(training_loader):
+        loss = model.forward(batch)
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+    loss_val = evaluation(val_loader, model_best=model, epoch=e)
+    nll_val.append(loss_val)
+    if e == 0:
+        torch.save(model, result_dir + name + '.model')
+        best_nll = loss_val
+    else:
+        if loss_val < best_nll:
+            torch.save(model, result_dir + name + '.model')
+            best_nll = loss_val
+            patience = 0
+
+            samples_generated(result_dir + name,
+                              val_loader,
+                              extra_name="_epoch_" + str(e))
+        else:
+            patience = patience + 1
+
+    if patience > max_patience:
+        break
+
+nll_val = np.asarray(nll_val)
+
 test_loss = evaluation(name=result_dir + name, test_loader=test_loader)
 f = open(result_dir + name + '_test_loss.txt', "w")
 f.write(str(test_loss))
 f.close()
-samples_real(result_dir + name, test_loader)
+num_x = 4
+num_y = 4
+x = next(iter(test_loader)).detach().numpy()
+fig, ax = plt.subplots(num_x, num_y)
+for i, ax in enumerate(ax.flatten()):
+    plottable_image = np.reshape(x[i], (8, 8))
+    ax.imshow(plottable_image, cmap='gray')
+    ax.axis('off')
+plt.savefig(result_dir + name + '_real_images.pdf', bbox_inches='tight')
+plt.close()
+
 samples_generated(result_dir + name, test_loader, extra_name='FINAL')
-plot_curve(result_dir + name, nll_val)
+plt.plot(np.arange(len(nll_val)), nll_val, linewidth='3')
+plt.xlabel('epochs')
+plt.ylabel('score matching loss')
+plt.savefig(result_dir + name + '_sm_val_curve.pdf', bbox_inches='tight')
+plt.close()
